16df00c1d16044b615e8c83abe33d3bf45035e79
Mrudhukar <mrudhukar@chronus.com>
2012-04-08 17:55:38 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dcare::Application.config.secret_token = '8a5653b5a71cb4656871be6be7499bcfa97ad6ec27cde0670b59a2d35e1138dc075a37438b9d8c7cf0964296a4dfc0516c614f06af7be476915691b0dc102269'
