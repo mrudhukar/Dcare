e6463435970629a53a5f51ae358e94e08d5d0556
Mrudhukar <mrudhukar@chronus.com>
2012-04-21 23:25:23 +0530

    Adding forems support

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,37 @@ class User < ActiveRecord::Base
 
   validates :name, :date_of_birth, :gender, :presence => true
   validates :gender, :inclusion => {:in => GENDER}
+
+  def to_s
+    name
+  end
+
+  def can_read_forem_category?(category)
+  	true
+  end
+
+  def can_read_forem_forums?
+  	true
+  end
+
+  def can_read_forem_forum?(forum)
+  	true
+  end
+
+  def can_create_forem_topics?(forum)
+  	true
+  end
+
+  def can_read_forem_topic?(forum)
+  	true
+  end
+
+  def can_reply_to_forem_topic?(topic)
+  	true
+  end
+
+  def can_edit_forem_posts?(forum)
+  	true
+  end
+
 end
